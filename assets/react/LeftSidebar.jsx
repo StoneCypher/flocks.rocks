22b912b8eb7480ec6ac7bf9d3ef40a6fa38ddce8
John Haugeland <stonecypher@gmail.com>
2015-02-12 03:11:54 -0800

    subcomponents

---
+++ assets/react/LeftSidebar.jsx
@@ -0,0 +1,18 @@
+
+/** @jsx React.DOM */
+
+"use strict";
+
+var React       = require("react"),
+
+    LeftSidebar = React.createClass({
+
+      render: function() {
+
+        return <div id="left-sidebar">test 2</div>;
+
+      }
+
+    });
+
+module.exports = LeftSidebar;
--- assets/react/RR.jsx
+++ assets/react/RR.jsx
@@ -4,6 +4,8 @@
 "use strict";
 
 var React = require("react"),
+    LSide = require("./LeftSidebar"),
+
     RR    = React.createClass({
 
       render: function() {
@@ -12,16 +14,15 @@ var React = require("react"),
 
             <div id="content">
 
-              <div id="left-sidebar">test</div>
+              <LSide/>
 
               <h1>Flocks Rocks!</h1>
                 <p>This will be the promotional webpage for <a href="https://github.com/StoneCypher/flocks.js">Flocks.JS</a>.</p>
                 <p>Flocks is an application layer for <a href="http://facebook.github.io/react/">React</a>; Flocks fulfills the same role and worldview as Flux, but with a radically different (and simpler) implementation strategy.</p>
 
-              <h1>This site hasn't yet been made.</h1>
+              <h1>This site has not yet been made.</h1>
 
-                <p>In the meantime, please enjoy these <a href="https://reactjsnews.com/making-react-make-sense/">tutorials</a> I'm writing.  They will also be hosted here momentarily.</p>
-                <p>Coming <i>very</i> soon now.</p>
+                <p>Tutorials coming <i>very</i> soon now.</p>
 
             </div>
 
