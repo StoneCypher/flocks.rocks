3174b2214e99f8ad6012a9773e74dc1cbe887044
John Haugeland <stonecypher@gmail.com>
2015-02-12 00:24:19 -0800

    basic publish infrastructure

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+
+require('gulp-awspublish');
+
+
+
+
+
+var max_age = 300;
+
+
+
+
+
+gulp.task('aws-push', function() {
+
+  var headers = {'Cache-Control' : 'max-age=' + max_age.toString()},
+      target  = aws.create({
+        'key'    : process.env.FLOCKSROCKS_ACCESS_KEY_ID,
+        'secret' : process.env.FLOCKSROCKS_SECRET_ACCESS_KEY,
+        'bucket' : process.env.FLOCKSROCKS_BUCKET
+      });
+
+  return gulp.src(dirs.publish_root + '/**/*')
+    .pipe(target.publish(headers))
+    .pipe(aws.reporter());
+
+});
