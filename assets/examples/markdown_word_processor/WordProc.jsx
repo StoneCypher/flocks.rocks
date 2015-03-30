4a4c7326dcd9f9fd8042259f78d4255c16d80586
John Haugeland <stonecypher@gmail.com>
2015-03-29 18:06:13 -0700

    Word Processor first steps

---
+++ assets/examples/markdown_word_processor/WordProc.jsx
@@ -0,0 +1,13 @@
+
+/** @jsx React.DOM */
+/* jshint node: true, browser: true */
+
+
+
+// the game of life renderable grid
+
+var WordProc = flocks.createClass({
+
+  return <div>Hello, world!</div>;
+
+});
