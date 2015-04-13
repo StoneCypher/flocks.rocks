a44bc0af4ca60eee19c3504ca490af02e17996a2
John Haugeland <stonecypher@gmail.com>
2015-04-12 22:02:43 -0700

    further

--- assets/examples/simple_bar_graph/BarGraph.jsx
+++ assets/examples/simple_bar_graph/BarGraph.jsx
@@ -8,25 +8,57 @@ var BarGraph = flocks.createClass({
 
   render: function() {
 
-    var style = {
+    var colCount = (this.props.data || []).length,
+        colWidth = ((101 - colCount) / colCount).toFixed(5) + '%',
 
-      width       : this.props.width      || '300px',
-      height      : this.props.height     || '200px',
-      hgap        : this.props.gap        || '3px',
-      vgap        : this.props.gap        || '2px',
-      border      : this.props.axis_style || 'solid',
-      borderWidth : this.props.axis_width || '0 0 thin thin',
-      borderColor : this.props.axis_color || 'black',
+        style = {
 
-    };
+          lineHeight  : '100%',
 
-    var barStyle = {
-      backgroundColor : this.props.bar_color  || '#24a9e1'
-    };
+          margin      : this.props.margin    || '0',
+          padding     : this.props.padding   || '0',
+          width       : this.props.width     || '300px',
+          height      : this.props.height    || '200px',
+/*
+          hgap        : this.props.gap       || '3px',
+          vgap        : this.props.gap       || '2px',
+*/
+          border      : this.props.axisStyle || 'solid',
+          borderWidth : this.props.axisWidth || '0 0 thin thin',
+          borderColor : this.props.axisColor || 'black'
+
+        },
+
+        barStyle = {
+          width           : colWidth,
+          textAlign       : 'center',
+          verticalAlign   : 'bottom',
+          display         : 'inline-block',
+          fontFamily      : this.props.fontFamily || 'sans-serif',
+          fontSize        : this.props.fontSize   || '70%',
+          color           : this.props.color      || 'white',
+          margin          : this.props.margin     || '0',
+          padding         : this.props.padding    || '0',
+          backgroundColor : this.props.barColor   || '#24a9e1'
+        };
+
+/*
+      .b1   { height: 33pt; }
+      .b2   { height: 37pt; }
+      .b3   { height: 14pt; }
+      .b4   { height: 28pt; }
+      .b5   { height: 17pt; }
+
+      .bar + .bar { margin: 0 0 0 1.5pt; }
+*/
 
     return (
 
       <div style={style}>
+        <div style={barStyle}>123</div>
+        <div style={barStyle}>123</div>
+        <div style={barStyle}>123</div>
+        <div style={barStyle}>123</div>
         <div style={barStyle}>123</div>
       </div>
 
